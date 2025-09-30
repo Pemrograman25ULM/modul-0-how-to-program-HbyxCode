2da08bd32fa0386ffc7933c5a95c797e4258f34d
HbyxCode <m.habibi2239@gmail.com>
2025-09-30 10:59:42 +0800

    Add files via upload

---
+++ Python/PRAK002-2510817310009-MuhammadHabibi.py
@@ -0,0 +1,5 @@
+NAMA = "MUHAMMAD HABIBI"
+
+print("Selamat Pagi, " + NAMA)
+print("Selamat Siang, " + NAMA)
+print("Selamat Malam, " + NAMA)
---
+++ Python/PRAK005-2510817310009-MuhammadHabibi.py
@@ -0,0 +1,11 @@
+huruf_hi = """
+#     #   #######
+#     #      #   
+#######      #   
+#     #      #   
+#     #      #   
+#     #   #######
+"""
+
+# Mencetak variabel yang berisi gambar tersebut
+print(huruf_hi)
